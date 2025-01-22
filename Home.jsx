5e98640786414456b3617760c7927ac10aefa599
Indirapriya05 <153306838+Indirapriya05@users.noreply.github.com>
2025-01-22 15:57:14 +0530

    Add files via upload

---
+++ Home.jsx
@@ -0,0 +1,12 @@
+import './Home.css';
+const Home = () =>{
+    return (
+    <div className="home-design">
+    <h1 className='home-title'>Home Page is Here....</h1>
+
+
+    <p className='home-description'>this is the simple homepage.you can navigate and design what you feel</p>
+    </div>
+    )
+};
+export default Home;
